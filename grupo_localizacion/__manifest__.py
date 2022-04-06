bb91d3831d419cce2618d98dce8323c7199b2535
Luis-omegasoft <luis.rondon@omegasoftve.com>
2022-04-06 08:26:38 -0400

    P6

---
+++ grupo_localizacion/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    MoviTrack
+#    Copyright (C) 2020-TODAY MoviTrack.
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Localizacion Grupo",
+    "version": "14.0.1",
+    "author": "Localizacion Venezolana",
+    "category": "grupos",
+    'depends': ['base'],
+    "data": [
+        'security/security.xml'
+    ],
+    'active': True,
+    'application': True,
+}
